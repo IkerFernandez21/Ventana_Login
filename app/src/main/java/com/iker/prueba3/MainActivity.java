1853cd4a10526ee12afa55177f04c24e5bb87c50
am2DAM2 <am2DAM2@informatica.iesjc>
2021-10-07 08:43:07 +0200

    Initial commit

---
+++ app/src/main/java/com/iker/prueba3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.iker.prueba3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+*   @author IkerFernandez21
+*
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
