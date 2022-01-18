e54f9d3f6b69c5ccccd9378a06ae0cc8862e328e
IkerFernandez21 <Ikeriratxe2003>
2022-01-18 08:38:43 +0100

    appbar

---
+++ app/src/main/java/com/iker/prueba3/MainBab.java
@@ -0,0 +1,13 @@
+package com.iker.prueba3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainBab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bab);
+    }
+}
